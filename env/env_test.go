654e10aaab18f3f75baa0b655fad1a24d48810d3
Michael Krelin <hacker@klever.net>
2019-12-21 02:10:12 +0100

    env: let Bool() return false even if truth is out there (#11)
    addresses micromdm/go4#10 issue, and add handling of false values
    just as we handle the truth.

--- env/env_test.go
+++ env/env_test.go
@@ -40,22 +40,42 @@ func TestBool(t *testing.T) {
 	var tests = []struct {
 		env   string
 		value bool
+		def   bool
+		unset bool
 	}{
-		{env: "TRUE", value: true},
-		{env: "true", value: true},
-		{env: "1", value: true},
-		{env: "F", value: false},
+		{env: "TRUE", def: true, value: true},
+		{env: "TRUE", def: false, value: true},
+		{env: "true", def: true, value: true},
+		{env: "true", def: false, value: true},
+		{env: "1", def: true, value: true},
+		{env: "1", def: false, value: true},
+
+		{env: "FALSE", def: true, value: false},
+		{env: "FALSE", def: false, value: false},
+		{env: "false", def: true, value: false},
+		{env: "false", def: false, value: false},
+		{env: "0", def: true, value: false},
+		{env: "0", def: false, value: false},
+
+		{env: "invalid", def: true, value: true},
+		{env: "invalid", def: false, value: false},
+
+		{unset: true, def: true, value: true },
+		{unset: true, def: false, value: false },
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.env, func(t *testing.T) {
 			key := "TEST_BOOL"
-			if err := os.Setenv(key, tt.env); err != nil {
-				t.Fatalf("failed to set env var %s for test: %s\n", key, err)
+			if tt.unset {
+				os.Unsetenv(key)
+			}else{
+				if err := os.Setenv(key, tt.env); err != nil {
+					t.Fatalf("failed to set env var %s for test: %s\n", key, err)
+				}
 			}
 
-			def := false
-			if have, want := Bool(key, def), tt.value; have != want {
+			if have, want := Bool(key, tt.def), tt.value; have != want {
 				t.Errorf("have %v, want %v", have, want)
 			}
 		})
